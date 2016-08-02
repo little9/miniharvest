87bcb371fb56fe76c2233b22638c9f51039f6a8e
James Little <j.little@miami.edu>
2016-08-02 14:28:47 -0400

    Updating tests

--- lib/miniharvest.rb
+++ lib/miniharvest.rb
@@ -1,48 +1,73 @@
 require "miniharvest/version"
 
+
 module MiniHarvest
   require 'net/http'
   require 'nokogiri'
-  require 'open-uri'
+  require 'logger'
 
   class MiniHarvest 
-     
-    attr_accessor :oai_base_uri, :set, :from, :marc_collection
+    
+    attr_accessor :oai_base_uri, :set, :from, :marc_collection, :logger, :initial_res, :initial_token
+    
+    def initialize(oai_base_uri, set, from)
+      @oai_base_uri = oai_base_uri
+      @set = set
+      @from = from
+      @marc_collection = Nokogiri::XML('<collection xmlns="http://www.loc.gov/MARC21/slim">')
+      @logger = Logger.new(STDOUT)
 
-     def append_to_collection(records)
-       self.marc_collection.root.add_child(records)
+      params = { :verb => 'ListRecords', :set => self.set, :metadataPrefix => 'marc21', :from => self.from }
+      @initial_res = oai_request(self.oai_base_uri,params)
+      @initial_token = get_resumption_token(@initial_res)
+    end
+    
+    def append_to_collection(records)
+      self.marc_collection.root.add_child(records)
     end
 
     def oai_request(uri,params)
       oai_uri = URI(uri)
       oai_uri.query = URI.encode_www_form(params)
-      return Net::HTTP.get_response(oai_uri)
-    end
+      @logger.info(oai_uri.query)
 
-    def process_request(res)
+      res = Net::HTTP.get_response(oai_uri)
+
       doc = Nokogiri::XML(res.body)
       doc.remove_namespaces!
-      records =  doc.xpath('//record')
-      append_to_collection(records)
-      
-      resumption_token =  doc.xpath('//resumptionToken')
-      return resumption_token.text
+
+      return doc
     end
-    
-    def initial_request
-      params = { :verb => 'ListRecords', :set => self.set, :metadataPrefix => 'marc21', :from => self.from }
-      res = oai_request(self.oai_base_uri,params)
-      get_records(process_request(res))
+
+    def get_resumption_token(doc)
+      resumption_token =  doc.xpath('//resumptionToken')
+      if resumption_token.length == 1
+        return resumption_token
+      else
+        return false
+      end
+    end
+
+    def process_request(doc)
+      token = get_resumption_token(doc)
+      if token != false
+        records =  doc.xpath('//record')
+        append_to_collection(records)
+        return token
+      else
+        return false
+      end
     end
 
     def get_records(token)
-      params = { :verb => 'ListRecords', :resumptionToken => token }
-      res = oai_request(self.oai_base_uri,params)
-      resumption_token = process_request(res)
-      if resumption_token != ""
+      params = { :verb => 'ListRecords', :resumptionToken => token[0].text }
+      doc = oai_request(self.oai_base_uri,params)
+      resumption_token = process_request(doc)
+      
+      if resumption_token != false
         get_records(resumption_token)
       else
-        return self.marc_collection
+        return self.marc_collection.root
       end
     end
   end
--- test/miniharvest_test.rb
+++ test/miniharvest_test.rb
@@ -1,15 +1,32 @@
 require 'test_helper'
 
 class MiniHarvestTest < Minitest::Test
+
+  def setup
+    @mh = MiniHarvest::MiniHarvest.new('https://na03.alma.exlibrisgroup.com/view/oai/01UOML_INST/request', 'udvd', '2016-07-01')
+  end
+
   def test_that_it_has_a_version_number
     refute_nil ::MiniHarvest::VERSION
   end
 
-  def test_that_you_get_some_xml
-    mh = MiniHarvest::MiniHarvest.new
-    mh.oai_base_uri = 'https://memory.loc.gov/cgi-bin/oai2_0'
-    mh.set = 'musdibib'
-    doc = mh.get_records(mh.initial_request)
-    refute_nil doc
+  def test_it_should_initilize_with_the_first_response
+    assert_kind_of(Nokogiri::XML::Document,@mh.initial_res)
   end
+
+  def test_that_it_should_have_some_records
+    refute_empty(@mh.initial_res.xpath('//record'))
+  end
+
+  def test_that_you_can_get_a_resumption_token
+    assert_kind_of(Nokogiri::XML::Node,@mh.get_resumption_token(@mh.initial_res)[0])
+  end
+
+  def test_that_you_dont_get_a_resumption_token_if_there_is_none
+    mh = MiniHarvest::MiniHarvest.new('http://sp.library.miami.edu/oai/oai.php', '', '')
+    assert_equal(false, mh.get_resumption_token(mh.initial_res))
+  end
+
+
+
 end
